b6346c79cd496c761c3124907704e4492c2b1237
tarek al azami <152443825+TAREK-AZM@users.noreply.github.com>
2025-01-01 17:50:37 +0100

    fix db connection

--- src/main/java/com/example/edoc/Utils/DatabaseConnection.java
+++ src/main/java/com/example/edoc/Utils/DatabaseConnection.java
@@ -6,16 +6,21 @@ import java.sql.DriverManager;
 public class DatabaseConnection {
     private static DatabaseConnection instance ;
     private Connection connection ;
-    private String url ="jdbc:postgresql://localhost:5432/edoc";
-    private String username ="postgres";
-    private final String password = "pgufc"  ;
+    //private String url ="jdbc:postgresql://localhost:5432/edoc";
+    private String url ="jdbc:mysql://localhost:3306/edoc";
+    //private String username ="postgres";
+    private String username ="root";
+    //private final String password = "pgufc"  ;
+    private final String password = ""  ;
+
 
 
 
     private DatabaseConnection(){
         try{
             // charger le driver de la connection
-            Class.forName("org.postgresql.Driver");
+            //Class.forName("org.postgresql.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             // creer la connection
             connection= DriverManager.getConnection(url, username, password) ;
             System.out.println("connection succeded");
